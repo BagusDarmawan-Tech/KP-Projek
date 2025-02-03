3a797a969e4877c85615443f9bf7a0d86827db3d
BagusDarmawan-Tech <bagusinformatika8@gmail.com>
2025-02-03 15:45:10 +0700

    menambahkan halaman admin

--- resources/views/forum-anak-galeri-anak.blade.php
+++ resources/views/forum-anak-galeri-anak.blade.php
@@ -21,7 +21,7 @@
      <nav class="navbar navbar-expand-lg navbar-light shadow-sm py-4 " id="mainNav">
         <div class="container px-5">
             <a class="navbar-brand" href="#">
-                <img src="logo.png" width="119" height="50" alt="">
+                <img src="{{ asset('logo.png') }}" width="119" height="50" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="bi-list"></i>
