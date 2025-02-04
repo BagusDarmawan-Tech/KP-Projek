33e8e62897a1bb66f4bc8874cf60861cd45cc25a
BagusDarmawan-Tech <bagusinformatika8@gmail.com>
2025-02-04 16:28:40 +0700

    Atur susunan Layouts Template Inheritance admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.admin-master');
+@section('main')
+
+<div class="pagetitle">
+    <h1>Dashboard</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+        <li class="breadcrumb-item active">Dashboard</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
+
+  <section class="section dashboard">
+    <div class="row">
+
+      <!-- Left side columns -->
+      
+    </div>
+  </section>
+
+@endsection
