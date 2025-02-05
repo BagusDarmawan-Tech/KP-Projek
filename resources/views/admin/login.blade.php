ec3e852a5f90e34b9ccd9f40879643a3bbe5af4e
BagusDarmawan-Tech <bagusinformatika8@gmail.com>
2025-02-05 16:03:52 +0700

    menambahkan menu fitur sidebar

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
   <body class="">
@@ -43,11 +44,11 @@
                         <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         
                         <div class="d-grid gap-2">
-    
                           <x-primary-button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3">
-                              {{ __('Log in') }}
+                              {{ __('Log in') }}        
+                              <i class="bi bi-box-arrow-in-right"></i>
+
                           </x-primary-button>
-                  
                         </div>
 
       
